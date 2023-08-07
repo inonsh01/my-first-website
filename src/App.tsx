d3963acf439ffa6fa8dddbe668636b78773ac597
inonsh01 <inonsh01@gmail.com>
2023-08-07 16:07:47 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Button from "./Button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+
+      <h2>This is my very cool f*cking website</h2>
+
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
+  );
+}
+
+export default App;
